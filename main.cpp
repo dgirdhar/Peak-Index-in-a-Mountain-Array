fbfec9d85cf6b8e49baa412b2b42e6fec3212480
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-07-10 14:32:04 -0400

    Update main.cpp
    Added binary search method to find out the max element.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
-        int previousItemIndex = 0;
+        int startIndex = 0;
+        int endIndex = A.size() - 1;
+        int res = 0;
         
-        for (int i = 1; i < A.size(); ++i) {
-            if (A[i] < A[previousItemIndex]) {
-                break;
+        while (startIndex <= endIndex) {
+            int mid = startIndex + (endIndex - startIndex) / 2;
+            
+            if ((mid + 1) <= endIndex && A[mid] < A[mid + 1]) {
+                startIndex = mid + 1;
+            }
+            else if ((mid - 1) >= startIndex && A[mid] < A[mid - 1]) {
+                endIndex = mid - 1;
             }
             else {
-                previousItemIndex = i;
+                res = mid;
+                break;
             }
         }
         
-        return previousItemIndex;
+        return res;
     }
 };
